3833db4743eef155f9c279c7ade3254ef913a51d
Andrey Pichugin <a-pichugin@yandex-team.ru>
2014-10-07 20:23:31 +0400

    now able to use different shader programs

--- src/com/datalink/basicopengles/BasicRenderer.java
+++ src/com/datalink/basicopengles/BasicRenderer.java
@@ -14,7 +14,7 @@ import android.os.SystemClock;
 
 public class BasicRenderer implements Renderer{
 
-    private final FloatBuffer mTriangle1Vertices;
+    private final FloatBuffer mPolygonVertices;
     private final int mBytesPerFloat = 4;
     
     private float[] mViewMatrix = new float[16];
@@ -26,20 +26,73 @@ public class BasicRenderer implements Renderer{
     private int mPositionHandle;
     private int mColorHandle;
 
-    private final int mStrideBytes = 7 * mBytesPerFloat;
-
     private final int mPositionOffset = 0;
     private final int mPositionDataSize = 3;
 
     private final int mColorOffset = 3;
     private final int mColorDataSize = 4;
     
+    private final int mStrideBytes = (mPositionDataSize + mColorDataSize) * mBytesPerFloat;
+    
     // lesson 2
     
 	private final FloatBuffer mCubePositions;
 	private final FloatBuffer mCubeColors;
-	//private final FloatBuffer mCubeNormals;
+	private final FloatBuffer mCubeNormals;
+	
+	private final float[] mLightPosInModelSpace = {0.0f, 0.0f, 0.0f, 1.0f};
+	/** Used to hold the current position of the light in world space (after transformation via model matrix). */
+	private final float[] mLightPosInWorldSpace = new float[4];
+	/** Used to hold the transformed position of the light in eye space (after transformation via modelview matrix) */
+	private final float[] mLightPosInEyeSpace = new float[4];
+	
+	private int mProgramHandle;
+	private int mPointProgramHandle;
     
+	float[] crossProduct(float[] fst, float[] sec, boolean normalize)
+	{
+	    //TODO: proper signs
+        float new0 = -fst[1]*sec[2] + fst[2]*sec[1];
+        float new1 = -fst[2]*sec[0] + fst[0]*sec[2];
+        float new2 = fst[1]*sec[0] - fst[0]*sec[1];
+        if( normalize )
+        {
+            float length = Matrix.length(new0, new1, new2);
+            float[] result = { new0/length, new1/length, new2/length};
+            return result;
+        }
+        
+        float[] result = { new0, new1, new2 };
+        return result;
+    }
+	
+	float[] normals(float[] vertices, int strideSize)
+	{
+	    int verticesInGroup = 3;
+	    
+	    int len = vertices.length / (strideSize*verticesInGroup);
+	    int size = len*verticesInGroup*mPositionDataSize;
+	    float[] result = new float[size];
+	    float[] fst = new float[mPositionDataSize];
+	    float[] sec = new float[mPositionDataSize];
+	    for (int i = 0; i < size; i+=strideSize*verticesInGroup)
+	    {
+	        for (int j = 0; j < mPositionDataSize; j++)
+	        {
+	            //TODO: proper signs
+	            fst[j] = vertices[i+j] - vertices[i+strideSize+j];
+	            sec[j] = vertices[i+strideSize*2+j] - vertices[i+strideSize+j];
+	        }
+	        
+	        float[] cross = crossProduct(fst, sec, true);
+	        for (int k = 0; k < verticesInGroup; k++)
+	        {
+	            System.arraycopy(cross, 0, result, i+k*strideSize,  mPositionDataSize);
+	        }
+	    }
+	    return result;
+	}
+
 	FloatBuffer bufferBoilerplate(final float[] values)
 	{
 	    FloatBuffer result = ByteBuffer.allocateDirect(values.length * mBytesPerFloat)
@@ -51,7 +104,7 @@ public class BasicRenderer implements Renderer{
     public BasicRenderer()
     {
         // This triangle is red, green, and blue.
-        final float[] triangleVerticesData = 
+        final float[] polygonVertices = 
         {
             // X, Y, Z,
             // R, G, B, A
@@ -74,9 +127,10 @@ public class BasicRenderer implements Renderer{
             0.0f, 1.0f, 0.0f, 1.0f
         };
         
-        mTriangle1Vertices = bufferBoilerplate(triangleVerticesData);
+        mPolygonVertices = bufferBoilerplate(polygonVertices);
 		mCubePositions = bufferBoilerplate(CubeData.positionArray);	
 		mCubeColors = bufferBoilerplate(CubeData.collorArray);
+		mCubeNormals = bufferBoilerplate( normals(CubeData.positionArray, mPositionDataSize) );
         
     }
  
@@ -112,17 +166,15 @@ public class BasicRenderer implements Renderer{
         // view matrix. In OpenGL 2, we can keep track of these matrices separately if we choose.
         Matrix.setLookAtM(mViewMatrix, 0, eyeX, eyeY, eyeZ, lookX, lookY, lookZ, upX, upY, upZ);
 
-        int vertexShaderHandle = shaderBoilerplate(GLES20.GL_VERTEX_SHADER, vertexShader);
-        int fragmentShaderHandle = shaderBoilerplate(GLES20.GL_FRAGMENT_SHADER, fragmentShader);
+        final int vertexShaderHandle = shaderBoilerplate(GLES20.GL_VERTEX_SHADER, vertexShader);
+        final int fragmentShaderHandle = shaderBoilerplate(GLES20.GL_FRAGMENT_SHADER, fragmentShader);
 
-        int programHandle = programBoilerplate(vertexShaderHandle, fragmentShaderHandle);
-    
-        mMVPMatrixHandle = GLES20.glGetUniformLocation(programHandle, "u_MVPMatrix");
-        mPositionHandle = GLES20.glGetAttribLocation(programHandle, "a_Position");
-        mColorHandle = GLES20.glGetAttribLocation(programHandle, "a_Color");
-
-        // Tell OpenGL to use this program when rendering.
-        GLES20.glUseProgram(programHandle);
+        mProgramHandle = programBoilerplate(vertexShaderHandle, fragmentShaderHandle, new String[] { "a_Position", "a_Color"});
+        
+        final int pointVertexShaderHandle = shaderBoilerplate(GLES20.GL_VERTEX_SHADER, pointVertexShader);
+        final int pointFragmentShaderHandle = shaderBoilerplate(GLES20.GL_FRAGMENT_SHADER, pointFragmentShader);
+        
+        mPointProgramHandle = programBoilerplate(pointVertexShaderHandle, pointFragmentShaderHandle, new String[] { "a_Position" });
     }
 
     @Override
@@ -148,6 +200,10 @@ public class BasicRenderer implements Renderer{
     public void onDrawFrame(GL10 glUnused)
     {
         GLES20.glClear(GLES20.GL_DEPTH_BUFFER_BIT | GLES20.GL_COLOR_BUFFER_BIT);
+        
+        mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgramHandle, "u_MVPMatrix");
+        mPositionHandle = GLES20.glGetAttribLocation(mProgramHandle, "a_Position");
+        mColorHandle = GLES20.glGetAttribLocation(mProgramHandle, "a_Color");
 
         // Do a complete rotation every 10 seconds.
         long time = SystemClock.uptimeMillis() % 10000L;
@@ -156,7 +212,7 @@ public class BasicRenderer implements Renderer{
         // Draw the triangle facing straight on.
         Matrix.setIdentityM(mModelMatrix, 0);
         Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 0.0f, 1.0f);
-        drawVertices(mTriangle1Vertices);
+        drawVertices(mPolygonVertices);
         
         // Draw some cubes.        
         Matrix.setIdentityM(mModelMatrix, 0);
@@ -182,10 +238,19 @@ public class BasicRenderer implements Renderer{
         Matrix.translateM(mModelMatrix, 0, 0.0f, 0.0f, -5.0f);
         Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 1.0f, 1.0f, 0.0f);        
         drawVertices(mCubePositions, mCubeColors);
+        
+        Matrix.setIdentityM(mModelMatrix, 0);
+        Matrix.translateM(mModelMatrix, 0, 0.0f, 0.0f, -5.0f);      
+        Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 1.0f, 0.0f);
+        Matrix.translateM(mModelMatrix, 0, 0.0f, 0.0f, 2.0f);
+        drawLight();
     }
 
     private void drawVertices(final FloatBuffer coloredPositionsBuffer)
     {
+        // Tell OpenGL to use this program when rendering.
+        GLES20.glUseProgram(mProgramHandle);
+        
         // Pass in the position information
         coloredPositionsBuffer.position(mPositionOffset);
         GLES20.glVertexAttribPointer(mPositionHandle, mPositionDataSize, GLES20.GL_FLOAT, false,
@@ -212,12 +277,12 @@ public class BasicRenderer implements Renderer{
         
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, coloredPositionsBuffer.capacity() / (mPositionDataSize + mColorDataSize));
     }
-    
-    /**
-	 * Draws a cube.
-	 */			
+    	
 	private void drawVertices(final FloatBuffer positionsBuffer, final FloatBuffer colorsBuffer)
-	{		
+	{	
+	    // Tell OpenGL to use this program when rendering.
+        GLES20.glUseProgram(mProgramHandle);
+        
 		// Pass in the position information
 		positionsBuffer.position(0);		
         GLES20.glVertexAttribPointer(mPositionHandle, mPositionDataSize, GLES20.GL_FLOAT, false,
@@ -244,6 +309,29 @@ public class BasicRenderer implements Renderer{
         
         // Draw the cube.
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, positionsBuffer.capacity() / mPositionDataSize);                               
+	}
+	
+	
+	private void drawLight()
+	{
+	    GLES20.glUseProgram(mPointProgramHandle);
+	
+		final int pointMVPMatrixHandle = GLES20.glGetUniformLocation(mPointProgramHandle, "u_MVPMatrix");
+        final int pointPositionHandle = GLES20.glGetAttribLocation(mPointProgramHandle, "a_Position");
+        
+		// Pass in the position.
+		GLES20.glVertexAttrib3f(pointPositionHandle, mLightPosInModelSpace[0], mLightPosInModelSpace[1], mLightPosInModelSpace[2]);
+
+		// Since we are not using a buffer object, disable vertex arrays for this attribute.
+        GLES20.glDisableVertexAttribArray(pointPositionHandle);  
+		
+		// Pass in the transformation matrix.
+		Matrix.multiplyMM(mMVPMatrix, 0, mViewMatrix, 0, mModelMatrix, 0);
+		Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mMVPMatrix, 0);
+		GLES20.glUniformMatrix4fv(pointMVPMatrixHandle, 1, false, mMVPMatrix, 0);
+		
+		// Draw the point.
+		GLES20.glDrawArrays(GLES20.GL_POINTS, 0, 1);
 	}
 
     final String vertexShader =
@@ -272,6 +360,24 @@ public class BasicRenderer implements Renderer{
             + "   gl_FragColor = v_Color;     \n"     // Pass the color directly through the pipeline.
             + "}                              \n";
     
+        // Define a simple shader program for our point.
+    final String pointVertexShader =
+    	"uniform mat4 u_MVPMatrix;      \n"		
+      +	"attribute vec4 a_Position;     \n"		
+      + "void main()                    \n"
+      + "{                              \n"
+      + "   gl_Position = u_MVPMatrix   \n"
+      + "               * a_Position;   \n"
+      + "   gl_PointSize = 5.0;         \n"
+      + "}                              \n";
+    
+    final String pointFragmentShader = 
+    	"precision mediump float;       \n"					          
+      + "void main()                    \n"
+      + "{                              \n"
+      + "   gl_FragColor = vec4(1.0,    \n" 
+      + "   1.0, 1.0, 1.0);             \n"
+      + "}                              \n";
         
     int shaderBoilerplate(int shaderType, String shaderProgram)
     {
@@ -302,7 +408,7 @@ public class BasicRenderer implements Renderer{
         return shaderHandle;
     }
     
-    int programBoilerplate(int vertexShaderHandle, int fragmentShaderHandle)
+    int programBoilerplate(final int vertexShaderHandle, final int fragmentShaderHandle, final String[] attributes)
     {
         int programHandle = GLES20.glCreateProgram();
         String why = "Error creating shader program. ";
@@ -311,8 +417,18 @@ public class BasicRenderer implements Renderer{
         {
             GLES20.glAttachShader(programHandle, vertexShaderHandle);
             GLES20.glAttachShader(programHandle, fragmentShaderHandle);
+            
+			if (attributes != null)
+			{
+				for (int i = 0; i < attributes.length; i++)
+				{
+					GLES20.glBindAttribLocation(programHandle, i, attributes[i]);
+				}						
+			}
+            
             GLES20.glBindAttribLocation(programHandle, 0, "a_Position");
             GLES20.glBindAttribLocation(programHandle, 1, "a_Color");
+            
             GLES20.glLinkProgram(programHandle);
          
             // Get the link status.
